1311272c1788a1ab6416b89d886b2d50b5e37dad
rawcptr <59281378+rawcptr@users.noreply.github.com>
2025-05-18 13:54:07 +0530

    fmt

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,14 +4,13 @@ local servers = { "html", "cssls", "nixd", "nushell", "clangd" }
 
 vim.lsp.config.clangd = {
     cmd = {
-        'clangd',
-        '--clang-tidy',
-        '--background-index'
+        "clangd",
+        "--clang-tidy",
+        "--background-index",
     },
-    root_markers = {'.clangd', 'compile_commands.json'},
-    filetypes = {'c', 'cpp'},
+    root_markers = { ".clangd", "compile_commands.json" },
+    filetypes = { "c", "cpp" },
 }
 
 vim.lsp.enable(servers)
-
 -- read :h vim.lsp.config for changing options of lsp servers
